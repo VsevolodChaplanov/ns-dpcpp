ef9f759761be1a3d2ba6c9d14bbc344d9e34f46e
VsevolodChaplanov <woornad@gmail.com>
2023-10-10 02:20:38 +0300

    Needed: Resolve a problem with the device's targeting platform Add variable directory where SYCL is installedadd docker support to build project with sycl and cuda backend

---
+++ Dependencies.cmake
@@ -0,0 +1,40 @@
+include(cmake/CPM.cmake)
+
+# Done as a function so that updates to variables like
+# CMAKE_CXX_FLAGS don't propagate out to other
+# targets
+function(nsdpcpp_setup_dependencies)
+
+    # For each dependency, see if it's
+    # already been provided to us by a parent project
+
+    # if(NOT TARGET fmtlib::fmtlib)
+    #   cpmaddpackage("gh:fmtlib/fmt#10.1.1")
+    # endif()
+
+    # if(NOT TARGET spdlog::spdlog)
+    #   cpmaddpackage(
+    #     NAME
+    #     spdlog
+    #     VERSION
+    #     1.11.0
+    #     GITHUB_REPOSITORY
+    #     "gabime/spdlog"
+    #     OPTIONS
+    #     "SPDLOG_FMT_EXTERNAL ON")
+    # endif()
+
+    # if(NOT TARGET Catch2::Catch2WithMain)
+    #   cpmaddpackage("gh:catchorg/Catch2@3.3.2")
+    # endif()
+
+    # if (NOT TARGET boost::boost)
+    #     cpmaddpackage(
+    #         Name Boost
+    #         VERSION 1.82.0
+    #         GITHUB_REPOSITORY "boostorg/boost"
+    #         GIT_TAG "boost-1.82.0"
+    #     )
+    # endif()
+
+endfunction()
--- cmake/PopulateSYCL.cmake
+++ cmake/PopulateSYCL.cmake
@@ -1,1 +1,7 @@
+set(CMAKE_C_COMPILER
+    "clang"
+    CACHE STRING "Only clang++ supports sycl")
+set(CMAKE_C++_COMPILER
+    "clang++"
+    CACHE STRING "Only clang++ supports sycl")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsycl")
